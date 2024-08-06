9704777f3e72930f6f34dbaf8f16c6b979abfc7c
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-06 17:26:29 +0200

    feat: menu items defined at config and implemented at menu of sidebar

--- src/app/dashboard/components/side-bar/side-bar.component.ts
+++ src/app/dashboard/components/side-bar/side-bar.component.ts
@@ -2,18 +2,24 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { SidebarModule } from 'primeng/sidebar';
+import { MENU_ITEMS } from '../../config/MenuItems';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
   imports: [
     SidebarModule,
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+
+  menuItems = MENU_ITEMS;
+
   isExpanded = false;
 
   toggleSidebar() {
---
+++ src/app/dashboard/config/MenuItems.ts
@@ -0,0 +1,28 @@
+import { MenuItem } from "primeng/api";
+
+export const MENU_ITEMS: MenuItem[] = [
+  {
+    title: 'Plantaciones',
+    route: '/plantation',
+    icon: 'pi pi-sun',
+    role: ['EMPLOYEE','BOSS']
+  },
+  {
+    title: 'Facturación',
+    route: '/facturation',
+    icon: 'pi pi-chart-bar',
+    role: ['BOSS']
+  },
+  {
+    title: 'Pedidos',
+    route: '/orders',
+    icon: 'pi pi-shopping-cart',
+    role: ['EMPLOYEE','BOSS']
+  },
+  {
+    title: 'Ajustes',
+    route: '/settings',
+    icon: 'pi pi-wrench',
+    role: ['EMPLOYEE','BOSS']
+  }
+];
